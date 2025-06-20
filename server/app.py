b06e99f9df374e1471e3010f496a8e703c1837d2
robertkutwa <robertkutwa0@gmail.com>
2025-06-20 22:59:11 +0300

    all passed

--- server/app.py
+++ server/app.py
@@ -1,55 +1,51 @@
-#!/usr/bin/env python3
+# app.py
 
-from flask import Flask, jsonify, request, make_response
-from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
+from flask import Flask, request, jsonify
 from models import db, Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
+# Initialize the database
 db.init_app(app)
 
-api = Api(app)
+# GET /plants - Get all plants
+@app.route('/plants', methods=['GET'])
+def get_plants():
+    plants = Plant.query.all()
+    return jsonify([plant.to_dict() for plant in plants]), 200
 
+# GET /plants/<id> - Get one plant
+@app.route('/plants/<int:id>', methods=['GET'])
+def get_plant(id):
+    plant = Plant.query.get_or_404(id)
+    return jsonify(plant.to_dict()), 200
 
-class Plants(Resource):
+# PATCH /plants/<id> - Update a plant's 'is_in_stock' status
+@app.route('/plants/<int:id>', methods=['PATCH'])
+def update_plant(id):
+    plant = Plant.query.get_or_404(id)
+    
+    data = request.get_json()
+    
+    if 'is_in_stock' in data:
+        plant.is_in_stock = data['is_in_stock']
+    
+    db.session.commit()
+    
+    return jsonify(plant.to_dict()), 200
 
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+# DELETE /plants/<id> - Delete a plant
+@app.route('/plants/<int:id>', methods=['DELETE'])
+def delete_plant(id):
+    plant = Plant.query.get_or_404(id)
+    
+    db.session.delete(plant)
+    db.session.commit()
+    
+    return '', 204
 
-    def post(self):
-        data = request.get_json()
-
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
-
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
-
-
-api.add_resource(PlantByID, '/plants/<int:id>')
-
-
+# Run the app
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,3 +1,5 @@
+# models.py
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -7,10 +9,10 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    image = db.Column(db.String)
-    price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    is_in_stock = db.Column(db.Boolean, default=True)
 
     def __repr__(self):
         return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
